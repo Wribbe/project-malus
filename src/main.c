a88a0f1f37124c266352eefc7dc9fe0df32bf896
Stefan Eng <eng_steff@outlook.com>
2020-09-29 21:43:46 +0200

    Empty glfw window working.

--- src/main.c
+++ src/main.c
@@ -6,9 +6,56 @@
 
 #include "utils.h"
 
+void
+render(void)
+{
+  glClearColor(0.2f, 0.2f, 0.2f, 1.0f);
+  glClear(GL_COLOR_BUFFER_BIT);
+}
+
+
 int
 main(void)
 {
-  printf("Hello World Again.\n");
+
+  if (!glfwInit()) {
+    printf("Could not load glfw, aborting\n");
+    return(EXIT_FAILURE);
+  }
+
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+  glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
+  size_t width = 800, height = 600;
+
+  GLFWwindow * window = glfwCreateWindow(
+    width,
+    height,
+    "Project Malus",
+    NULL,
+    NULL
+  );
+
+  glfwMakeContextCurrent(window);
+  if (window == NULL) {
+    printf("Failed to create glfw-window, aborting.\n");
+    return EXIT_FAILURE;
+  }
+
+  if (!gladLoadGL()) {
+    printf("Could not load opengl-bindings, aborting.\n");
+    return EXIT_FAILURE;
+  }
+
+  printf("OpenGL Version %d.%d loaded.\n", GLVersion.major, GLVersion.minor);
+
+  while (!glfwWindowShouldClose(window)) {
+    glfwPollEvents();
+    render();
+    glfwSwapBuffers(window);
+  }
+
   res_load_obj("temp_file_name.obj");
 }
